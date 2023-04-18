e2cb5a033b1af9f6732cdd79297fd649305a1ba9
ZARIFMAC <uzbzarif@gmail.com>
2023-04-18 16:28:22 +0500

    add two text field

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,34 @@
 import 'package:flutter/material.dart';
 
-TextEditingController controller_ = TextEditingController(text: '1\n2\n3\n');
+TextEditingController controller_A = TextEditingController(text: '0');
+TextEditingController controller_B = TextEditingController(text: '0');
 
-void fn() {
-  print(controller_.text);
-}
+void fn() {}
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
-            margin: EdgeInsets.all(15),
+            margin: const EdgeInsets.all(15),
             child: TextField(
-              keyboardType: TextInputType.multiline,
-              minLines: 3,
-              maxLines: 5,
-              controller: controller_,
-              decoration: InputDecoration(
+              controller: controller_A,
+              decoration: const InputDecoration(
                 border: OutlineInputBorder(),
-                // label: Text('First name'),
-                labelText: 'ABC',
+                labelText: 'A',
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(15),
+            child: TextField(
+              controller: controller_B,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'B',
               ),
             ),
           ),
