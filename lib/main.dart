63dfa4b23fac404492a7206d74e48c96d1e478c2
ZARIFMAC <uzbzarif@gmail.com>
2023-04-18 15:43:38 +0500

    add button

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
 
+void fn() {}
 void main() {
+  TextEditingController controller_ = TextEditingController(text: '1\n2\n3\n');
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Scaffold(
-      body: Container(
-        margin: EdgeInsets.all(15),
-        child: TextField(
-          keyboardType: TextInputType.multiline,
-          minLines: 3,
-          maxLines: 5,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            // label: Text('First name'),
-            labelText: 'ABC',
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.all(15),
+            child: TextField(
+              keyboardType: TextInputType.multiline,
+              minLines: 3,
+              maxLines: 5,
+              controller: controller_,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                // label: Text('First name'),
+                labelText: 'ABC',
+              ),
+            ),
           ),
-        ),
+          TextButton(onPressed: fn, child: Text('Button'))
+        ],
       ),
     ),
   ));
