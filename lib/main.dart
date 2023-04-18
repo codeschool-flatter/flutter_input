f11c159b95f3b6d43032a38cb8c562328d70ca81
ZARIFMAC <uzbzarif@gmail.com>
2023-04-18 15:00:56 +0500

    add border

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Scaffold(
       body: Container(
+        margin: EdgeInsets.all(15),
         child: TextField(
           maxLength: 5,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            label: Text('First name'),
+          ),
         ),
       ),
     ),
